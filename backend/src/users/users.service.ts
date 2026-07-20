e9bfe725aff2e890eff60ee206cba326c0c0b7e0
Felipe F. <felipee.fonseca.09@gmail.com>
2026-07-20 13:29:53 -0300

    prisma version 5.22.0

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@nestjs/common';
-const { PrismaClient } = require('../generated'); //ta com erro, entender como consertar depois
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  // Inicializa o Prisma direto no serviço, seguindo o roteiro à risca
   private prisma = new PrismaClient();
 
   async create(data: any) {
-    // Cria o usuário no banco com os dados recebidos da rota
     const newUser = await this.prisma.user.create({
       data: {
         name: data.name,
         email: data.email,
-        password: data.password, // Em produção usaremos criptografia, mas para o protótipo inicial salvamos direto
+        password: data.password,
         institution: data.institution,
-        role: 'USER', // Valor padrão exigido pelo schema
+        role: 'USER',
       },
     });
 
