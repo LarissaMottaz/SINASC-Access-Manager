9980193adf61a3f91d8a1c02d5aedd3c8e65aad3
Larissa Motta <larissamottazx@gmail.com>
2026-08-03 20:34:20 -0300

    atualizacao de pastas

--- backend/src/ai-settings/ai-settings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AiSettingsService } from './ai-settings.service';
-import { AiSettingsController } from './ai-settings.controller';
-
-@Module({
-  providers: [AiSettingsService],
-  controllers: [AiSettingsController]
-})
-export class AiSettingsModule {}
