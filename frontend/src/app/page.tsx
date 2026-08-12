a89510b1cb366142e46c44288171ef5ed9cbf478
Larissa Motta <larissamottazx@gmail.com>
2026-08-11 22:45:19 -0300

    Primeiro commit no repositório SINASC - Larissa

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,44 +1,39 @@
 import styles from "./home.module.css";
+import {
+  ArrowRightStartOnRectangleIcon,
+  UserPlusIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
 
 export default function Home() {
+
   return (
+
     <main className={styles.container}>
-      //testeeeeeeeeeeeeeeeeeeeee
-      
-
       <div className={styles.card}>
-
         <h1>SINASC Access Manager</h1>
-
         <div className={styles.line}></div>
-
         <p>
           Sistema para gerenciamento de usuários, autenticação e auditoria
           de acessos.
         </p>
-
         <div className={styles.buttons}>
+          <Link href="/login" className={styles.primaryButton}>
+            <ArrowRightStartOnRectangleIcon className={styles.icon} />
+            Entrar
+          </Link>
 
-          <a href="/login">
-            <button className={styles.primaryButton}>
-              Entrar
-            </button>
-          </a>
-
-          <a href="/register">
-            <button className={styles.secondaryButton}>
-              Cadastrar
-            </button>
-          </a>
-
+          <Link href="/register" className={styles.secondaryButton}>
+            <UserPlusIcon className={styles.icon} />
+            Cadastrar
+          </Link>
         </div>
-
       </div>
-      
+
        <footer className={styles.footer}>
         © 2026 SINASC Access Manager
     </footer>
-
     </main>
   );
 }
